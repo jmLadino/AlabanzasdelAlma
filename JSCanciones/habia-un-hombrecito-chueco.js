a91b17888904556d9d5c46a8328d54512f5c8e3a
“jmLadino” <josemiguel.ladino@gmail.com>
2023-09-19 22:24:22 -0300

    Se agregan 10 temas

---
+++ JSCanciones/habia-un-hombrecito-chueco.js
@@ -0,0 +1,52 @@
+﻿const contenidoHTML_Letra = `	<div class="image-container">		<span class="tooltip">			<img id="btnLetra" src="https://jmladino.github.io/AlabanzasdelAlma/imgmicrofono.png" alt="Letra" class="imagebutton">			<span class="tooltip-content">Letra</span>		</span>				<span class="tooltip">			<img id="btnGuitarra" src="https://jmladino.github.io/AlabanzasdelAlma/imgguitarra.png" alt="Guitarra" class="imagebutton">			<span class="tooltip-content">Guitarra</span>		</span>		<span class="tooltip">			<img id="btnUkelele" src="https://jmladino.github.io/AlabanzasdelAlma/imgukelele.png" alt="Ukelele" class="imagebutton">					<span class="tooltip-content">Ukelele</span>		</span>	</div>	<p class="InterlineadoLetra">Había un hombrecito chueco<br />vivía en una casa chueca<br />andaba por caminos chuecos<br />su vida era chueca<br /></p><p class="InterlineadoLetra">Un día el hombrecito chueco<br />la Biblia encontró<br />// y todo lo que era chueco<br />Jesús enderezó //<br /></p>`;const DIV_LETRA = document.getElementById("DIVLetra");DIV_LETRA.innerHTML = contenidoHTML_Letra;const contenidoHTML_Acordes = `	<div class="image-container">
+		<span class="tooltip">
+			<img id="btnLetra2" src="https://jmladino.github.io/AlabanzasdelAlma/imgmicrofono.png" alt="Letra" class="imagebutton">
+			<span class="tooltip-content">Letra</span>
+		</span>
+		
+		<span class="tooltip">
+			<img id="btnGuitarra2" src="https://jmladino.github.io/AlabanzasdelAlma/imgguitarra.png" alt="Guitarra" class="imagebutton">
+			<span class="tooltip-content">Guitarra</span>
+		</span>
+
+		<span class="tooltip">
+			<img id="btnUkelele2" src="https://jmladino.github.io/AlabanzasdelAlma/imgukelele.png" alt="Ukelele" class="imagebutton">		
+			<span class="tooltip-content">Ukelele</span>
+		</span>
+	</div>	
+                <br />
+                <div id="DIV_Ribbon">
+                  <span class="tooltip"><button class="blue-button" onclick="transposeNotes(1)">T+</button><span class="tooltip-content">Subir Medio Tono</span></span>
+                  <span class="tooltip"><button class="blue-button" onclick="transposeNotes(-1)">T-</button><span class="tooltip-content">Bajar Medio Tono</span></span>
+                  <span class="tooltip"><button class="blue-button" onclick="TonalidadInicial()">T0</button><span class="tooltip-content">Tonalidad Inicial</span></span>
+                  <span class="tooltip"><button class="blue-button" onclick="convertirNotacion()">Do / C</button><span class="tooltip-content">Alternar Notación</span></span>
+                  <button id="btnAcordes" class="blue-button">Acordes</button>
+                </div>
+
+                <div id="container">
+                    <div class="modal-overlay" id="modalOverlay">
+                        <div class="modal-content">
+                            <div id="imageContainer" class="fixed-div"></div>
+                            <button id="closeModalButton">Cerrar</button>
+                        </div>
+                    </div>
+
+                    <p class="InterlineadoGuitarra">
+Ha<span class="chord" data-tabsaurus="Do"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Do.png' alt="Do" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">bía<span class="note" data-notation="Latina">Do</span></span></span> un hombrecito chueco <br />
+vi<span class="chord" data-tabsaurus="Fa"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Fa.png' alt="Fa" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">vía<span class="note" data-notation="Latina">Fa</span></span></span> en una casa chueca <br />
+andaba por caminos chuecos <br />
+su <span class="chord" data-tabsaurus="Sol"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Sol.png' alt="Sol" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">vi<span class="note" data-notation="Latina">Sol</span></span></span>da era <span class="chord" data-tabsaurus="Do"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Do.png' alt="Do" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">chue<span class="note" data-notation="Latina">Do</span></span></span>ca <br />
+</p><p class="InterlineadoGuitarra">
+Un <span class="chord" data-tabsaurus="Do"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Do.png' alt="Do" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">día<span class="note" data-notation="Latina">Do</span></span></span> el hombrecito chueco <br />
+la Biblia <span class="chord" data-tabsaurus="Do7"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Do7.png' alt="Do7" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">en<span class="note" data-notation="Latina">Do7</span></span></span>con<span class="chord" data-tabsaurus="Fa"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Fa.png' alt="Fa" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">tró<span class="note" data-notation="Latina">Fa</span></span></span> <br />
+// y <span class="chord" data-tabsaurus="Fa"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Fa.png' alt="Fa" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">to<span class="note" data-notation="Latina">Fa</span></span></span>do lo que era <span class="chord" data-tabsaurus="Do"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Do.png' alt="Do" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">chue<span class="note" data-notation="Latina">Do</span></span></span>co <br />
+Je<span class="chord" data-tabsaurus="Sol"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Sol.png' alt="Sol" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">sús<span class="note" data-notation="Latina">Sol</span></span></span> endere<span class="chord" data-tabsaurus="Do"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Do.png' alt="Do" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">zó<span class="note" data-notation="Latina">Do</span></span></span> //<br />
+</p>
+                </div>
+`;
+const DIV_Acordes = document.getElementById("DIVAcordes");
+DIV_Acordes.innerHTML = contenidoHTML_Acordes;
+const contenidoHTML_Youtube = ``;
+const DIV_Youtube = document.getElementById("DIVYoutube");
+DIV_Youtube.innerHTML = contenidoHTML_Youtube;
+const contenidoHTML_Cursor = `  <span class="tooltipPrevNext"><a href="https://alabanzasdelalma.blogspot.com/2023/06/era-un-muchachito-de-nombre-david.html"><button class="blue-button">Anterior</button></a><span class="tooltipPrevNext-content">14. Era un muchachito de nombre David</span></span>&emsp;  <a href="https://alabanzasdelalma.blogspot.com/2023/06/indice.html">      <button class="blue-button">INDICE</button>  </a>  &emsp;  <span class="tooltipPrevNext"><a href="https://alabanzasdelalma.blogspot.com/2023/06/jonas.html"><button class="blue-button">Siguiente</button></a><span class="tooltipPrevNext-content">16. Jonás</span></span>`;const DIV_CURSOR = document.getElementById("DIVCursor");DIV_CURSOR.innerHTML = contenidoHTML_Cursor;
