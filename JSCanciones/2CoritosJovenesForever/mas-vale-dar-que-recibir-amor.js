62d73a973c381ebc50e811f374aff9bb78c68041
jmLadino <josemiguel.ladino@gmail.com>
2025-05-16 23:41:29 -0400

    Se agregan cantos

---
+++ JSCanciones/2CoritosJovenesForever/mas-vale-dar-que-recibir-amor.js
@@ -0,0 +1,53 @@
+﻿const contenidoHTML_Letra = `	<div class="image-container">		<span class="tooltip">			<img id="btnLetra" src="https://jmladino.github.io/AlabanzasdelAlma/imgmicrofono.png" alt="Letra" class="imagebutton">			<span class="tooltip-content">Letra</span>		</span>				<span class="tooltip">			<img id="btnGuitarra" src="https://jmladino.github.io/AlabanzasdelAlma/imgguitarra.png" alt="Guitarra" class="imagebutton">			<span class="tooltip-content">Guitarra</span>		</span>		<span class="tooltip">			<img id="btnUkelele" src="https://jmladino.github.io/AlabanzasdelAlma/imgukelele.png" alt="Ukelele" class="imagebutton">					<span class="tooltip-content">Ukelele</span>		</span>	</div>	<p class="InterlineadoLetra">Más vale dar que recibir<br />Más vale dar que recibir<br />Más vale dar que recibir amor<br />Una mirada, una sonrisa,<br />Una amistad, más vale dar que recibir<br />Más vale dar amor<br />Más vale dar amor<br /></p>`;const DIV_LETRA = document.getElementById("DIVLetra");DIV_LETRA.innerHTML = contenidoHTML_Letra;const contenidoHTML_Acordes = `	<div class="image-container">
+		<span class="tooltip">
+			<img id="btnLetra2" src="https://jmladino.github.io/AlabanzasdelAlma/imgmicrofono.png" alt="Letra" class="imagebutton">
+			<span class="tooltip-content">Letra</span>
+		</span>
+		
+		<span class="tooltip">
+			<img id="btnGuitarra2" src="https://jmladino.github.io/AlabanzasdelAlma/imgguitarra.png" alt="Guitarra" class="imagebutton">
+			<span class="tooltip-content">Guitarra</span>
+		</span>
+
+		<span class="tooltip">
+			<img id="btnUkelele2" src="https://jmladino.github.io/AlabanzasdelAlma/imgukelele.png" alt="Ukelele" class="imagebutton">		
+			<span class="tooltip-content">Ukelele</span>
+		</span>
+	</div>	
+                <br />
+                <div id="DIV_Ribbon">
+                  <span class="tooltip"><button class="blue-button" onclick="transposeNotes(1)">T+</button><span class="tooltip-content">Subir Medio Tono</span></span>
+                  <span class="tooltip"><button class="blue-button" onclick="transposeNotes(-1)">T-</button><span class="tooltip-content">Bajar Medio Tono</span></span>
+                  <span class="tooltip"><button class="blue-button" onclick="TonalidadInicial()">T0</button><span class="tooltip-content">Tonalidad Inicial</span></span>
+                  <span class="tooltip"><button class="blue-button" onclick="convertirNotacion()">Do / C</button><span class="tooltip-content">Alternar Notación</span></span>
+                  <button id="btnAcordes" class="blue-button">Acordes</button>
+                  <span class="tooltip"><button class="buttonSizeText" onclick="SizeText('aumentar')">A+</button><span class="tooltip-content">Aumentar tamaño del texto</span></span>
+                  <span class="tooltip"><button class="buttonSizeText" onclick="SizeText('disminuir')">A-</button><span class="tooltip-content">Disminuir tamaño del texto</span></span>
+                </div>
+
+                <div id="container">
+                    <div class="modal-overlay" id="modalOverlay">
+                        <div class="modal-content">
+                            <div id="imageContainer" class="fixed-div"></div>
+                            <button id="closeModalButton">Cerrar</button>
+                        </div>
+                    </div>
+
+                    <p class="InterlineadoGuitarra">
+<span class="chord" data-tabsaurus="Mi"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Mi.png' alt="Mi" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">Más<span class="note" data-notation="Latina">Mi</span></span></span> vale <span class="chord" data-tabsaurus="La"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/La.png' alt="La" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">dar<span class="note" data-notation="Latina">La</span></span></span> que reci<span class="chord" data-tabsaurus="Si"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Si.png' alt="Si" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">bir<span class="note" data-notation="Latina">Si</span></span></span><br />
+Más vale <span class="chord" data-tabsaurus="Solm"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Solm.png' alt="Solm" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">dar<span class="note" data-notation="Latina">Solm</span></span></span> que reci<span class="chord" data-tabsaurus="Do#m"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/DoSostm.png' alt="Do#m" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">bir<span class="note" data-notation="Latina">Do#m</span></span></span><br />
+Más vale <span class="chord" data-tabsaurus="Fa#m"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/FaSostm.png' alt="Fa#m" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">dar<span class="note" data-notation="Latina">Fa#m</span></span></span> que reci<span class="chord" data-tabsaurus="Si"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Si.png' alt="Si" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">bir<span class="note" data-notation="Latina">Si</span></span></span> a<span class="chord" data-tabsaurus="Mi"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Mi.png' alt="Mi" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">mor<span class="note" data-notation="Latina">Mi</span></span></span><br />
+Una mi<span class="chord" data-tabsaurus="La"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/La.png' alt="La" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">ra<span class="note" data-notation="Latina">La</span></span></span>da, una son<span class="chord" data-tabsaurus="Si"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Si.png' alt="Si" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">ri<span class="note" data-notation="Latina">Si</span></span></span>sa,<br />
+Una amis<span class="chord" data-tabsaurus="Solm"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Solm.png' alt="Solm" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">tad<span class="note" data-notation="Latina">Solm</span></span></span>, más vale <span class="chord" data-tabsaurus="Do#m"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/DoSostm.png' alt="Do#m" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">dar<span class="note" data-notation="Latina">Do#m</span></span></span> que reci<span class="chord" data-tabsaurus="Fa#m"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/FaSostm.png' alt="Fa#m" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">bir<span class="note" data-notation="Latina">Fa#m</span></span></span><br />
+Más vale <span class="chord" data-tabsaurus="Si"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Si.png' alt="Si" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">dar<span class="note" data-notation="Latina">Si</span></span></span> a<span class="chord" data-tabsaurus="Mi"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Mi.png' alt="Mi" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">mor<span class="note" data-notation="Latina">Mi</span></span></span><br />
+Más vale <span class="chord" data-tabsaurus="La"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/La.png' alt="La" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">dar<span class="note" data-notation="Latina">La</span></span></span> que reci<span class="chord" data-tabsaurus="Si"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Si.png' alt="Si" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">bir<span class="note" data-notation="Latina">Si</span></span></span> a<span class="chord" data-tabsaurus="Mi"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Mi.png' alt="Mi" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">mor<span class="note" data-notation="Latina">Mi</span></span></span><br />
+</p>
+                </div>
+`;
+const DIV_Acordes = document.getElementById("DIVAcordes");
+DIV_Acordes.innerHTML = contenidoHTML_Acordes;
+const contenidoHTML_Youtube = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/4sDIa25nOO4?si=MPFhjYax6pc0GNdA" title="Más vale dar que recibir, amor" frameborder="0" allowfullscreen></iframe>
+`;
+const DIV_Youtube = document.getElementById("DIVYoutube");
+DIV_Youtube.innerHTML = contenidoHTML_Youtube;
+
