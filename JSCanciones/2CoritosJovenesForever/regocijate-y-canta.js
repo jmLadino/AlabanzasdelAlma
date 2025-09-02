6c580d46d7887be80685e52180933d6ea8da23fb
jmLadino <josemiguel.ladino@gmail.com>
2025-09-02 00:38:42 -0400

    Se sube el canto 122

---
+++ JSCanciones/2CoritosJovenesForever/regocijate-y-canta.js
@@ -0,0 +1,59 @@
+﻿const contenidoHTML_Letra = `	<div class="image-container">		<span class="tooltip">			<img id="btnLetra" src="https://jmladino.github.io/AlabanzasdelAlma/imgmicrofono.png" alt="Letra" class="imagebutton">			<span class="tooltip-content">Letra</span>		</span>				<span class="tooltip">			<img id="btnGuitarra" src="https://jmladino.github.io/AlabanzasdelAlma/imgguitarra.png" alt="Guitarra" class="imagebutton">			<span class="tooltip-content">Guitarra</span>		</span>		<span class="tooltip">			<img id="btnUkelele" src="https://jmladino.github.io/AlabanzasdelAlma/imgukelele.png" alt="Ukelele" class="imagebutton">					<span class="tooltip-content">Ukelele</span>		</span>	</div>	<p class="CSSAutor"><a href="https://www.bible.com/es/bible/149/ISA.12.6" target="_blank">Isaías 12:6</a></p><p class="InterlineadoLetra">Regocíjate y canta,<br />oh moradora de Sion.<br />Regocíjate y canta,<br />oh moradora de Sion;<br /></p><p class="InterlineadoLetra">Porque grande es<br />porque grande es<br />porque grande es en medio de ti<br />el Santo de Israel.<br />Porque grande es<br />porque grande es<br />porque grande es en medio de ti<br />el Santo de Israel.<br /></p>`;const DIV_LETRA = document.getElementById("DIVLetra");DIV_LETRA.innerHTML = contenidoHTML_Letra;const contenidoHTML_Acordes = `	<div class="image-container">
+		<span class="tooltip">
+			<img id="btnLetra2" src="https://jmladino.github.io/AlabanzasdelAlma/imgmicrofono.png" alt="Letra" class="imagebutton">
+			<span class="tooltip-content">Letra</span>
+		</span>
+		
+		<span class="tooltip">
+			<img id="btnGuitarra2" src="https://jmladino.github.io/AlabanzasdelAlma/imgguitarra.png" alt="Guitarra" class="imagebutton">
+			<span class="tooltip-content">Guitarra</span>
+		</span>
+
+		<span class="tooltip">
+			<img id="btnUkelele2" src="https://jmladino.github.io/AlabanzasdelAlma/imgukelele.png" alt="Ukelele" class="imagebutton">		
+			<span class="tooltip-content">Ukelele</span>
+		</span>
+	</div>	
+<p class="CSSAutor">Isaías 12:6</p>
+                <div id="DIV_Ribbon">
+                  <span class="tooltip"><button class="blue-button" onclick="transposeNotes(1)">T+</button><span class="tooltip-content">Subir Medio Tono</span></span>
+                  <span class="tooltip"><button class="blue-button" onclick="transposeNotes(-1)">T-</button><span class="tooltip-content">Bajar Medio Tono</span></span>
+                  <span class="tooltip"><button class="blue-button" onclick="TonalidadInicial()">T0</button><span class="tooltip-content">Tonalidad Inicial</span></span>
+                  <span class="tooltip"><button class="blue-button" onclick="convertirNotacion()">Do / C</button><span class="tooltip-content">Alternar Notación</span></span>
+                  <button id="btnAcordes" class="blue-button">Acordes</button>
+                  <span class="tooltip"><button class="buttonSizeText" onclick="SizeText('aumentar')">A+</button><span class="tooltip-content">Aumentar tamaño del texto</span></span>
+                  <span class="tooltip"><button class="buttonSizeText" onclick="SizeText('disminuir')">A-</button><span class="tooltip-content">Disminuir tamaño del texto</span></span>
+                </div>
+
+                <div id="container">
+                    <div class="modal-overlay" id="modalOverlay">
+                        <div class="modal-content">
+                            <div id="imageContainer" class="fixed-div"></div>
+                            <button id="closeModalButton">Cerrar</button>
+                        </div>
+                    </div>
+
+                    <p class="InterlineadoGuitarra">
+Rego<span class="chord" data-tabsaurus="Lam"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Lam.png' alt="Lam" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">cí<span class="note" data-notation="Latina">Lam</span></span></span>jate y canta,<br />
+oh moradora de Si<span class="chord" data-tabsaurus="Mi"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Mi.png' alt="Mi" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">on<span class="note" data-notation="Latina">Mi</span></span></span><br />
+Rego<span class="chord" data-tabsaurus="Mi"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Mi.png' alt="Mi" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">cí<span class="note" data-notation="Latina">Mi</span></span></span>jate y canta,<br />
+oh moradora de Si<span class="chord" data-tabsaurus="Lam"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Lam.png' alt="Lam" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">on<span class="note" data-notation="Latina">Lam</span></span></span>&emsp;<span class="chord" data-tabsaurus="La7"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/La7.png' alt="La7" style="height:140px;width: 100px;"></span><span class="syllable">&emsp;<span class="note" data-notation="Latina">La7</span></span>&emsp;</span><br />
+</p><p class="InterlineadoGuitarra">
+porque grande <span class="chord" data-tabsaurus="Rem"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Rem.png' alt="Rem" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">es<span class="note" data-notation="Latina">Rem</span></span></span><br />
+porque grande <span class="chord" data-tabsaurus="Lam"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Lam.png' alt="Lam" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">es<span class="note" data-notation="Latina">Lam</span></span></span><br />
+porque grande <span class="chord" data-tabsaurus="Mi"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Mi.png' alt="Mi" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">es<span class="note" data-notation="Latina">Mi</span></span></span> en medio de ti<br />
+el Santo de Is<span class="chord" data-tabsaurus="Lam"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Lam.png' alt="Lam" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">rael<span class="note" data-notation="Latina">Lam</span></span></span>&emsp;<span class="chord" data-tabsaurus="La7"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/La7.png' alt="La7" style="height:140px;width: 100px;"></span><span class="syllable">&emsp;<span class="note" data-notation="Latina">La7</span></span>&emsp;</span><br />
+</p><p class="InterlineadoGuitarra">
+Porque grande <span class="chord" data-tabsaurus="Rem"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Rem.png' alt="Rem" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">es<span class="note" data-notation="Latina">Rem</span></span></span><br />
+porque grande <span class="chord" data-tabsaurus="Lam"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Lam.png' alt="Lam" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">es<span class="note" data-notation="Latina">Lam</span></span></span><br />
+porque grande <span class="chord" data-tabsaurus="Mi"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Mi.png' alt="Mi" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">es<span class="note" data-notation="Latina">Mi</span></span></span> en medio de ti<br />
+el Santo de Is<span class="chord" data-tabsaurus="Lam"><span class="chord-tooltip"><img src='https://jmladino.github.io/AlabanzasdelAlma/FullChords/Lam.png' alt="Lam" style="height:140px;width: 100px;"></span><span class="syllable" style="text-decoration: underline;">rael<span class="note" data-notation="Latina">Lam</span></span></span><br />
+</p>
+                </div>
+`;
+const DIV_Acordes = document.getElementById("DIVAcordes");
+DIV_Acordes.innerHTML = contenidoHTML_Acordes;
+const contenidoHTML_Youtube = ``;
+const DIV_Youtube = document.getElementById("DIVYoutube");
+DIV_Youtube.innerHTML = contenidoHTML_Youtube;
+
