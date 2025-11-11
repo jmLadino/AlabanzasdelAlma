8720c6ead01027050ce8568abcbd8950e3e6201f
jmLadino <josemiguel.ladino@gmail.com>
2025-11-11 13:12:42 -0300

    Se agrega link YT

--- JSCanciones/5AutorInterprete/DuoZimrah/herido-por-nuestro-dolor.js
+++ JSCanciones/5AutorInterprete/DuoZimrah/herido-por-nuestro-dolor.js
@@ -78,7 +78,8 @@ Y así nunca ol<span class="chord" data-tabsaurus="Mi"><span class="chord-toolti
 `;
 const DIV_Acordes = document.getElementById("DIVAcordes");
 DIV_Acordes.innerHTML = contenidoHTML_Acordes;
-const contenidoHTML_Youtube = ``;
+const contenidoHTML_Youtube = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/eh94EyLu49s?si=RtVaj07-W8hvDj6S" title="Herido por nuestro dolor" frameborder="0" allowfullscreen></iframe>
+`;
 const DIV_Youtube = document.getElementById("DIVYoutube");
 DIV_Youtube.innerHTML = contenidoHTML_Youtube;
 
